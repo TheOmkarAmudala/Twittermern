1a0b6fc202068856640dbbf6556f7328531a9fe2
Omkar Amudala <147320407+TheOmkarAmudala@users.noreply.github.com>
2024-06-08 11:39:58 +0530

    Update constants.js

--- TwitterFrontend/src/utensiles/constants.js
+++ TwitterFrontend/src/utensiles/constants.js
@@ -1,4 +1,4 @@
-export const API_KEY_USER = "http://https://twittermern-1wdc.vercel.app//api/v1/user"
-export const API_KEY_TWEET = "http://https://twittermern-1wdc.vercel.app//api/v1/tweet"
+export const API_KEY_USER = "https://twittermern-1wdc.vercel.app//api/v1/user"
+export const API_KEY_TWEET = "https://twittermern-1wdc.vercel.app//api/v1/tweet"
 
 
